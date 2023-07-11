1b4e6d94b786b99f8faf27b01ef97f91ea1dee76
skyviolet <mloida591@gmail.com>
2023-07-11 19:37:35 -0400

    game.finish

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const barra = document.querySelector("#barra");
-const value = document.querySelector("#value");
+const barra = document.querySelector("#barra")
+const value = document.querySelector("#value")
 const container = document.querySelector(".box")
-const color = document.querySelector(".color")
 
 function createGrids() {
   let size = barra.value;
   let gridWidth = (500 / size)+ "px";
   container.style.gridTemplateColumns = `repeat(${size}, ${gridWidth})`;
   container.style.gridTemplateRows = `repeat(${size}, ${gridWidth})`;
+
   for (let i=0; i<(size**2); i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.onclick = () => square.style.backgroundColor=`${color.value}`
+
+    const color = document.querySelector("#color")
+      color.addEventListener("click", () => {
+        square.onclick = () => square.style.backgroundColor=`${color.value}`
+      })
+
+    const randomColor = document.querySelector(".rainbow")
+      randomColor.addEventListener("click", () => { 
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        square.onclick = ()=> square.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+      })
+
+    const eraser = document.getElementById("eraser")
+      eraser.addEventListener("click", () => {
+        const deleteColor = "#ffffff";
+        square.onclick = ()=> square.style.backgroundColor = `${deleteColor}`
+      })
+
     container.appendChild(square)
+    }
   }
-}
+
+createGrids()
 
 function deleteGrids() {
   const grids = container.querySelectorAll(".square")
@@ -21,10 +44,14 @@ function deleteGrids() {
 }
 
 value.textContent = barra.value;
-
 barra.addEventListener("input", (event) => {
   value.textContent = event.target.value;
   deleteGrids()
   createGrids()
 })
 
+const deleteAll = document.querySelector(".deleteAll") 
+deleteAll.addEventListener("click", () => {
+    deleteGrids()
+    createGrids()
+})
